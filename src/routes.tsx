2a6f44143961de08233b2aa8430e036f6a5359b3
Eric Rocha <ericrochaeric@gmail.com>
2021-05-28 15:46:49 -0300

    init

---
+++ src/routes.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator }  from '@react-navigation/stack';
+
+import Catalog from './pages/Catalog';
+import Header from './components/Header';
+
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: true,
+          cardStyle:{ backgroundColor: '#313746'}
+        }}
+        initialRouteName="Catalog"
+      >
+        <Stack.Screen
+          name="Catalog"
+          component={Catalog}
+          options={{
+            headerTitleAlign:'center',
+            headerTransparent: true,
+            header: () => <Header />
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
